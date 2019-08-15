dd24c650936dd2264246d741096894258ed85a10
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-15 21:31:52 +0300

    Максимум из трех чисел [#190][#171219]

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 public class MultiMax {
 
     public int max(int first, int second, int third) {
-        return new Max().max(new Max().max(first, second), third);
+        Max max = new Max();
+        return max.max(max.max(first, second), third);
     }
 }
