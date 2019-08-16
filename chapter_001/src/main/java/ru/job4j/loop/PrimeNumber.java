d745f6642dca485646c34221337618bc6f96f806
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-16 18:21:48 +0300

    Простые числа. [#156314]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            for (int j = 2; j <= i; j++) {
+                if (i % j == 0 && i != j) {
+                    break;
+                } else if (i % j == 0 && i == j) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
