1df089f89482cb4d5ed3b5ba262e1bdd4369c4ac
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-16 19:18:45 +0300

    JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
 
+/**
+ * Package for calculator task.
+ *
+ * @author Alexandr Sakhatyr (alexandr.010@yandex.ru)
+ * @since 16.08.2019
+ */
 public class Calculator {
 
     public static void add(double first, double second) {
@@ -22,6 +28,10 @@ public class Calculator {
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * Method main
+     * @param args
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4,2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculator task.
+ *
+ * @author Alexandr Sakhatyr (alexandr.010@yandex.ru)
+ * @version $Id$
+ * @since 16.08.2019
+ */
+package ru.job4j.calculator;
