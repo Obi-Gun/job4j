a8bd0238459a0c358fac94c640ed420a32dce487
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-17 20:28:04 +0300

    Поиск индекса в диапазоне. [#156318]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -9,14 +9,45 @@ package ru.job4j.array;
 public class FindLoop {
 
     /**
-     * Method indexOf looking for an element in array and return index
+     * Method indexOf looking for an element in array and return index of an element
      * @param data - arr for search
      * @param el - element for search
-     * @return rst - index
+     * @return rst - index of an element
      */
     public int indexOf(int[] data, int el) {
         int rst = -1; // если элемента нет в массиве, то возвращаем -1.
         for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
+    /**
+     * Method indexOf looking for an element in array
+     * from start index to finish index and return index of an element
+     * @param data arr for search
+     * @param el element for search
+     * @param startIndex start index for search
+     * @param finishIndex finish index for search
+     * @return index of an element
+     */
+    public int indexOf(int[] data, int el, int startIndex, int finishIndex) {
+        if (startIndex > finishIndex) {
+            int memory = startIndex;
+            startIndex = finishIndex;
+            finishIndex = memory;
+        }
+        if (startIndex < 0) {
+            startIndex = 0;
+        }
+        if (finishIndex >= data.length) {
+            finishIndex = data.length - 1;
+        }
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int index = startIndex; index < data.length && index <= finishIndex; index++) {
             if (data[index] == el) {
                 rst = index;
                 break;
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -35,4 +35,40 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenFindThen3() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenDoesntFindThenMinus1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 5;
+        int start = 1;
+        int finish = 4;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFindButStartIndexAndFinishIndexIsOutOfArrayLengthThen7() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 2, 10, 2, 4, 9, 8, 7, 6, 3};
+        int value = 7;
+        int start = -77777;
+        int finish = 77777;
+        int result = find.indexOf(input, value, start, finish);
+        int expect = 7;
+        assertThat(result, is(expect));
+    }
 }
