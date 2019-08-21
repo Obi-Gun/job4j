7a1754d3c92152b0aebf387358b531d405108922
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-21 22:44:38 +0300

    Рефакторинг - Расстояние между точками. [#122587]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -2,16 +2,37 @@ package ru.job4j.condition;
 
 public class Point {
 
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    //to refactor
+    public Point() {
+    }
+
+    //to refactor
     public static double distance(int x1, int y1, int x2, int y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
-    public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
-        System.out.println("distance from (0, 0) to (2, 0) is " + result);
-        result = distance(5, 0, 10, 0);
-        System.out.println("distance from (5, 0) to (10, 0) is " + result);
-        result = distance(0, 0, 2, 2);
-        System.out.println("distance from (0, 0) to (2, 2) is " + result);
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
+
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
+//    public static void main(String[] args) {
+//        double result = distance(0, 0, 2, 0);
+//        System.out.println("distance from (0, 0) to (2, 0) is " + result);
+//        result = distance(5, 0, 10, 0);
+//        System.out.println("distance from (5, 0) to (10, 0) is " + result);
+//        result = distance(0, 0, 2, 2);
+//        System.out.println("distance from (0, 0) to (2, 2) is " + result);
+//    }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -3,6 +3,9 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class PointTest {
 
     @Test
@@ -14,5 +17,31 @@ public class PointTest {
         int expected = 5;
         int out = (int) Point.distance(inX1, inY1, inX2, inY2);
         Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenZeroAndTenThenTen() {
+        Point first = new Point(0, 0);
+        Point second = new Point(0, 10);
+        double result = first.distance(second);
+        first.info();
+        second.info();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(10D));
+    }
+
+    @Test
+    public void whenCheckItself() {
+        Point point = new Point(0, 0);
+        double result = point.distance(point);
+        assertThat(result, is(0D));
+    }
+
+    @Test
+    public void whenShowInfo() {
+        Point first = new Point(1, 1);
+        first.info();
+        Point second = new Point(2, 2);
+        second.info();
     }
 }
