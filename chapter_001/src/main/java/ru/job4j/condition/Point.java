5a1761c59d794b4ed7d8f54b0592893a032bfa41
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-23 00:10:41 +0300

    Расстояние между точками в трехмерном пространстве. [#122643]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,20 +1,54 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+/**
+ * Class for calculating distance between two points
+ *
+ * @author Alexandr Sakhatyr (alexandr.010@yandex.ru)
+ * @since 23.08.2019
+ */
 public class Point {
 
     private int x;
     private int y;
+    private int z;
 
-    public Point(int first, int second) {
-        this.x = first;
-        this.y = second;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    /**
+     * Method for calculating distance between two points in 2D space
+     * @param that - Point with x and y coordinates
+     * @return distance value
+     */
     public double distance(Point that) {
-        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    /**
+     * Method for calculating distance between two points in 3D space
+     * @param that - Point with x, y and z coordinates
+     * @return distance value
+     */
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public void info() {
-        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+        System.out.format("Point[%s, %s]\n", this.x, this.y);
+    }
+
+    public void info3d() {
+        System.out.format("Point[%s, %s, %s]\n", this.x, this.y, this.z);
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -7,6 +7,9 @@ import static org.hamcrest.Matchers.is;
 
 public class PointTest {
 
+    /**
+     * Testing method double distance(Point that)
+     */
     @Test
     public void whenZeroAndTenThenTen() {
         Point first = new Point(0, 0);
@@ -14,22 +17,57 @@ public class PointTest {
         double result = first.distance(second);
         first.info();
         second.info();
-        System.out.println(String.format("Result is %s", result));
+        System.out.format("Result is %s\n", result);
         assertThat(result, is(10D));
     }
 
     @Test
-    public void whenCheckItself() {
+    public void whenCheckItselfDistanceMethod() {
         Point point = new Point(0, 0);
         double result = point.distance(point);
         assertThat(result, is(0D));
     }
 
+    /**
+     *  Testing method double distance3d(Point that)
+     */
+    @Test
+    public void whenZeroAndTenThenTenDistance3dMethod() {
+        Point first = new Point(0, 0, 0);
+        Point second = new Point(0, 10, 0);
+        double result = first.distance3d(second);
+        first.info3d();
+        second.info3d();
+        System.out.format("Result is %s\n", result);
+        assertThat(result, is(10D));
+    }
+
+    @Test
+    public void whenCheckItselfDistance3dMethod() {
+        Point point = new Point(0, 0, 0);
+        double result = point.distance3d(point);
+        assertThat(result, is(0D));
+    }
+
+    /**
+     * Testing method void info()
+     */
     @Test
     public void whenShowInfo() {
         Point first = new Point(1, 1);
         first.info();
         Point second = new Point(2, 2);
         second.info();
+    }
+
+    /**
+     * Testing method void info3d()
+     */
+    @Test
+    public void whenShowInfo3d() {
+        Point first = new Point(1, 1, 1);
+        first.info3d();
+        Point second = new Point(2, 2, 2);
+        second.info3d();
     }
 }
