b426f03cac0a87fb97ec06c1a80b7a65e92e71e3
Alexandr Sakhatyr <alexandr.010@yandex.ru>
2019-08-25 23:02:13 +0300

    Конструктор по умолчанию. [#173889]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Predoctor.java
@@ -0,0 +1,9 @@
+package ru.job4j.tracker;
+
+public class Predoctor extends Animal {
+
+    public Predoctor() {
+//        super();
+        System.out.println("class Predoctor");
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker;
+
+public class Tiger extends Predoctor {
+
+    public Tiger() {
+//        super();
+        System.out.println("class Tiger");
+    }
+
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
